07bde75cd028f3c5cdd9fe479264dd44d971c2e2
Darantir <yakushev_1996@inbox.ru>
2018-02-25 23:20:10 +0300

    Darantir
    SnackBar

--- SecondLab/app/src/main/java/com/example/denis/secondlab/MainActivity.kt
+++ SecondLab/app/src/main/java/com/example/denis/secondlab/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.denis.secondlab
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget
+import android.support.design.widget.Snackbar
+import android.widget.Button
+//import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Double.parseDouble
 
@@ -13,7 +15,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.setOnClickListener{
+        val normal = findViewById<Button>(R.id.button4)
+        button.setOnClickListener {
 
 
             val s1 = editText1.text.toString();
@@ -22,13 +25,13 @@ class MainActivity : AppCompatActivity() {
             val a = parseDouble(s1);
             val b = parseDouble(s2);
 
-            val c = a+b;
+            val c = a + b;
 
             val s = c.toString();
 
             textView.text = s;
         }
-        button2.setOnClickListener{
+        button2.setOnClickListener {
 
 
             val s1 = editText1.text.toString();
@@ -37,13 +40,13 @@ class MainActivity : AppCompatActivity() {
             val a = parseDouble(s1);
             val b = parseDouble(s2);
 
-            val c = a-b;
+            val c = a - b;
 
             val s = c.toString();
 
             textView.text = s;
         }
-        button3.setOnClickListener{
+        button3.setOnClickListener {
 
 
             val s1 = editText1.text.toString();
@@ -52,13 +55,13 @@ class MainActivity : AppCompatActivity() {
             val a = parseDouble(s1);
             val b = parseDouble(s2);
 
-            val c = a*b;
+            val c = a * b;
 
             val s = c.toString();
 
             textView.text = s;
         }
-        button4.setOnClickListener{
+        button4.setOnClickListener {
 
 
             val s1 = editText1.text.toString();
@@ -67,18 +70,18 @@ class MainActivity : AppCompatActivity() {
             val a = parseDouble(s1);
             val b = parseDouble(s2);
 
-            if (b == 0.0){
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).show();
+            if (b == 0.0) {
+                Snackbar.make(normal, "Nononono", 10).show();
+
+            } else {
+                val c = a / b;
+
+                val s = c.toString();
+
+                textView.text = s;
             }
-            val c = a/b;
-
-            val s = c.toString();
-
-            textView.text = s;
         }
+
+
     }
-
-
-
-
 }
